803dcf147f102218133c8b124c80387b99e4a8fe
M4GNV5 <eragonjml@googlemail.com>
2016-07-13 22:53:14 +0200

    make ffcb_return_t a void* typedef and name some parameters nicer

--- src/ffcb.h
+++ src/ffcb.h
@@ -3,13 +3,13 @@
 
 #include <stdint.h>
 
-typedef void ffcb_return_t;
+typedef void *ffcb_return_t;
 
 void *ffcb_create(void *func, void *arg);
 void ffcb_delete(void *func);
 
-void ffcb_return_int(ffcb_return_t *func, intptr_t val);
-void ffcb_return_pointer(ffcb_return_t *func, void *val);
-void ffcb_return_float(ffcb_return_t *func, double value);
+void ffcb_return_int(ffcb_return_t ret, intptr_t val);
+void ffcb_return_pointer(ffcb_return_t ret, void *val);
+void ffcb_return_float(ffcb_return_t ret, double val);
 
 #endif
